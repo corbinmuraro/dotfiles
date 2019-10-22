c68f670b112672cd447629982a12411a95998687
corbin muraro <corbinmuraro@gmail.com>
2019-10-21 23:13:10 -0700

    add iterm configuration to setup script

--- setup.sh
+++ setup.sh
@@ -51,67 +51,73 @@ brew cleanup
 
 echo "Setting up Mac settings..."
 
-#"Disabling OS X Gate Keeper"
-#"(You'll be able to install any app you want from here on, not just Mac App Store apps)"
+# Disabling OS X Gate Keeper
+# (You'll be able to install any app you want from here on, not just Mac App Store apps)
 sudo spctl --master-disable
 sudo defaults write /var/db/SystemPolicy-prefs.plist enabled -string no
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-#"Automatically quit printer app once the print jobs complete"
+# Automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-#"Use column view in all Finder windows by default"
+# Use column view in all Finder windows by default
 defaults write com.apple.finder FXPreferredViewStyle Clmv
 
-#"Enabling snap-to-grid for icons on the desktop and in other icon views"
+# Enabling snap-to-grid for icons on the desktop and in other icon views
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
-#"Resize Dock items"
+# Resize Dock items
 defaults write com.apple.dock tilesize -int 36
 
-#"Setting Dock to auto-hide"
+# Setting Dock to auto-hide
 defaults write com.apple.dock autohide -bool true
 
-#"Preventing Time Machine from prompting to use new hard drives as backup volume"
+# Preventing Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-#"Use `~/Downloads/Incomplete` to store incomplete downloads"
+# Use `~/Downloads/Incomplete` to store incomplete downloads
 defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
 defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
 
-#"Setting screenshots location to ~/Desktop"
+# Setting screenshots location to ~/Desktop
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
-#"Setting screenshot format to PNG"
+# Setting screenshot format to PNG
 defaults write com.apple.screencapture type -string "png"
 
-#"Hiding Safari's bookmarks bar by default"
+# Hiding Safari's bookmarks bar by default
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
-#"Hiding Safari's sidebar in Top Sites"
+# Hiding Safari's sidebar in Top Sites
 defaults write com.apple.Safari ShowSidebarInTopSites -bool false
 
-#"Disabling Safari's thumbnail cache for History and Top Sites"
+# Disabling Safari's thumbnail cache for History and Top Sites
 defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
 
-#"Enabling Safari's debug menu"
+# Enabling Safari's debug menu
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-#"Making Safari's search banners default to Contains instead of Starts With"
+# Making Safari's search banners default to Contains instead of Starts With
 defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 
-#"Removing useless icons from Safari's bookmarks bar"
+# Removing useless icons from Safari's bookmarks bar
 defaults write com.apple.Safari ProxiesInBookmarksBar "()"
 
-#"Enabling the Develop menu and the Web Inspector in Safari"
+# Enabling the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
 killall Finder
 
